95a45bc0af1ccd8ee1a718be1b35e2cd9208be50
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2024-01-21 20:51:39 -0500

    Changing about section and redeployed with new env variable

--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -22,7 +22,7 @@ export default function Portfolio(){
         <div className="py-10">
           <div className="mt-8">
           <h2 className="font-bold text-4xl dark:text-green-500 text-blue-400">Projects</h2>
-          <p className="break-words font-light text-lg mt-10">The following projects below showcase my skill and experience that can be applied in real worlds. Each project is briefly describe with its functionality and live demos to it. It served as proof for my ability to solve complex problems and manage each projects effectively.</p>
+          <p className="break-words font-light text-lg mt-10">The following projects below showcase my skill and experience that can be applied in real worlds. Each project is briefly describe with its functionality and the source code for it linked to Github. It served as proof for my ability to solve complex problems and manage each projects effectively.</p>
         
           </div>
           <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-8 ">
